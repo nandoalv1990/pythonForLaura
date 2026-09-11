94f2aa61b817a74b5fdab962696fb46f335db1fe
nandoalv1990 <nandoalv1990@gmail.com>
2026-09-11 11:58:13 -0600

    .

---
+++ Programa con sí condicional básico.py
@@ -0,0 +1,10 @@
+# Programa con sí condicional básico
+opcion = int(input("Ingresa\n1) Para imprimir un poema\n2) Para imprimir un mensaje motivacional\nElige: "))
+
+if (opcion == 2):
+    print("Ya falta menos para conseguirlo...")
+if (opcion == 1):
+    print("Mis lágrimas tibias \nRecorren mi adolorido rostro\nCon el frío de tu partida...")
+print(f"\nElegiste la opción {opcion})")
+
+# Fin del programa
